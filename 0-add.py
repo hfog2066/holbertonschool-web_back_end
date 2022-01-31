49ce5751ec2e67f46525304a8a5637592236535f
hfog2066 <hfog2000@yahoo.com>
2022-01-31 15:59:24 -0500

    0. Basic annotations - add

--- 0-add.py
+++ 0-add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 """
-    Module with typing Python
+Basic annotations - add
 """
 
 
 def add(a: float, b: float) -> float:
     """
-        Args:
-            a: The first parameter
-            b: The second parameter
-
-        Return:
-            Sum of two floats
-
+    function to add two floats
     """
-
-    return (a + b)
+    return a + b
