5caa0fff031797c462fb24369d7bf2fa72094e9f
hfog2066 <hfog2000@yahoo.com>
2022-01-31 15:03:32 -0500

    0. Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a float a and a 
+float b as arguments and returns their sum as a float. 
+"""
+
+def add(a: float, b: float) -> float:
+    """
+       Args:
+           a: The first parameter
+           b: The second parameter
+
+       
+       Return:
+           Sum of two floats
+
+
+    """
+
+    return (a + b)
