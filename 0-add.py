0dd40a6c8a7824c76898170628d5e57a18a9b1fa
hfog2066 <hfog2000@yahoo.com>
2022-01-31 16:18:29 -0500

    0. Basic annotations - add

--- 0-add.py
+++ 0-add.py
@@ -2,10 +2,10 @@
 """"
 Module for 0. Basic annotations - add.
 0x00. Python - Variable Annotations
-Holberton Web Stack programming Spec ― Back-end
+
 """
 
 
 def add(a: float, b: float) -> float:
-    """Method that returns the sum of two floats"""
+    """function that returns the sum of two floats"""
     return a + b
