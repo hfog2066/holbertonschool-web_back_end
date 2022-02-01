e9c82363234990cfdef678407804fc502016bcbc
hfog2066 <hfog2000@yahoo.com>
2022-02-01 12:27:51 -0500

    4. Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,29 +4,24 @@ Module for 4. Tasks.
 0x01. Python - Async
 """
 import asyncio
-import random
 from typing import List
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    executes multiples coroutines at the same time
-    parameters n : int
-    max_delay : int
-    Returns List[float]
-    list of the delays in ascending order
+    Method that executes multiples coroutines at the same time
+
+    Parameters
+    ----------
+        n : int
+        max_delay : int
+    Returns
+    -------
+        List[float]
+            list of the delays in ascending order
     """
-    delays: List[float] = []
-    tasks: List[asyncio.Task] = []
-
-    for _ in range(n):
-        tasks.append(task_wait_random(max_delay))
-
-    for task in asyncio.as_completed((tasks)):
-        delay = await task
-        delays.append(delay)
-
-    return delays
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
