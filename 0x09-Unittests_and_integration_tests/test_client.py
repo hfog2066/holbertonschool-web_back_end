af92a7bdbbef92dc0dac9e075d1613db9c298e13
hfog2066 <hfog2000@yahoo.com>
2022-03-02 12:59:30 -0500

    4. Parameterize and patch as decorators

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Module for testing client """
 
 from client import GithubOrgClient
-from fixtures import TEST_PAYLOAD
+#from fixtures import TEST_PAYLOAD
 from parameterized import parameterized, parameterized_class
 import json
 import unittest
